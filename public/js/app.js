26e1cc17d2fa5f3ccc263bf461d61251805c4ad3
Jessica Meza <jessicamezarojas2011@gmail.com>
2018-02-15 06:41:27 -0500

    funcionalidad de la api weather listo

---
+++ public/js/app.js
@@ -0,0 +1,89 @@
+const init = function() {
+  const getTemperature = function() {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday'
+    ];
+
+    let currently = data.currently;
+    let weatherReportDaysOfTheWeek = data.daily.data;
+    let icon = currently.icon;
+    let dataTemperature = currently.temperature;
+    let dataSummary = currently.summary;
+    let dataWind = currently.windsSpeed;
+    let dataHumidity = currently.humidity;
+    let dataUvIndex = currently.uvIndex;
+    let dataPressure = currently.pressure;
+
+    let boxDaysOfTheWeek = document.querySelector('#box-days-of-the-week');
+    let iconWeather = document.querySelector('#icon-weather');
+    let temperature = document.querySelector('#temperature');
+    let summary = document.querySelector('#summary');
+    let humidity = document.querySelector('#humidity');
+    let uvIndex = document.querySelector('#uvIndex');
+    let pressure = document.querySelector('#pressure');
+
+
+    iconWeather.setAttribute('src', `../assets/images/${icon}.png`);
+    temperature.innerHTML = `${dataTemperature} °`;
+    summary.innerHTML = `SUMMARY ${dataSummary}`;
+    humidity.innerHTML = `${dataHumidity} %`;
+    uvIndex.innerHTML = dataUvIndex;
+    pressure.innerHTML = `${dataPressure} hPa`;
+
+    let contenido = weatherReportDaysOfTheWeek.slice(0, 7);
+
+    contenido.forEach((element, i) => {
+      let template = `<div class = "row">
+        <div><img src="../assets/images/${element.icon}.png"></div>
+        <div><p>${days[i]}</p></div>
+        <div><p>${element.temperatureMin}°</p></div>
+        <div><p>${element.temperatureMax}°</p></div>
+        </div>`;
+      boxDaysOfTheWeek.innerHTML += template;
+    });
+  };
+
+  const handleError = () => {
+    console.error('Se ha presentado un error!!');
+  };
+
+  const getLocation = () =>{
+    navigator.geolocation.getCurrentPosition(function(pos) {
+      let lat = pos.coords.latitude;
+      let lon = pos.coords.longitude;
+      console.log(lat + ' ' + lon);
+
+      const weatherReport = () => { 
+        let apiKey = '032c5a70a625c5ca43ffe56e18773429',
+          proxy = 'https://cors-anywhere.herokuapp.com/',
+          apiLinkDS = `https://api.darksky.net/forecast/${apiKey}/${lat},${lon}?units=si`,
+          url = proxy + apiLinkDS;
+      
+    
+        const XHR = new XMLHttpRequest();
+        
+        XHR.open('GET', url);
+        XHR.onload = getTemperature;
+        XHR.onerror = handleError;
+        console.log(url);
+        XHR.send();
+      };
+      weatherReport();
+    });
+  };
+
+  getLocation();
+
+};
+window.addEventListener('load', init);
+
+
